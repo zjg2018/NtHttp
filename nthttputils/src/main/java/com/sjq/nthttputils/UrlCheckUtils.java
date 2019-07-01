b11d16b047dec7832bdfa542d24489b569efb6c4
jingguo <380150073@qq.com>
2019-07-01 14:23:55 +0800

    V1.0

---
+++ nthttputils/src/main/java/com/sjq/nthttputils/UrlCheckUtils.java
@@ -0,0 +1,48 @@
+package com.sjq.nthttputils;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+public class UrlCheckUtils {
+
+    public final String GKIP = "10.101.0.90";
+
+    public String checkUrl(String url, int type) {
+        if (type == 1) {
+            boolean isexist = startPing("192.168.30.188");
+            if (isexist) {
+                url = url.replace(GKIP, "192.168.30.188");
+            } else {
+                isexist = startPing("10.101.0.90");
+                if (isexist) {
+                    url = url.replace(GKIP, "10.101.0.90");
+                }
+            }
+        }
+        return url;
+    }
+
+    private boolean startPing(String ip) {
+        boolean isexist = false;
+        Process process = null;
+
+        try {
+            process = Runtime.getRuntime().exec("ping -c 1 -i 0.5 -W 1 " + ip);
+            int status = process.waitFor();
+            if (status == 0) {
+                isexist = true;
+            } else {
+                isexist = false;
+            }
+        } catch (IOException e) {
+            isexist = false;
+        } catch (InterruptedException e) {
+            isexist = false;
+        } finally {
+            process.destroy();
+        }
+        Log.i("mmmm", "ping:" + ip + ",isexist:" + isexist);
+        return isexist;
+    }
+}
